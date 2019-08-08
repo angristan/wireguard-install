44ef24620ea21dd3f78fcd576adb5c29e8b818d6
angristan <angristan@pm.me>
2019-08-08 23:02:50 +0200

    Prevent word splitting
    Make CI happy (shellcheck - SC2046)

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -84,13 +84,13 @@ fi
 if [[ "$OS" = 'ubuntu' ]]; then
     add-apt-repository ppa:wireguard/wireguard
     apt-get update
-    apt-get install linux-headers-$(uname -r)
+    apt-get install "linux-headers-$(uname -r)"
     apt-get install wireguard iptables
 elif [[ "$OS" = 'debian' ]]; then
     echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
     printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
     apt update
-    apt-get install linux-headers-$(uname -r)
+    apt-get install "linux-headers-$(uname -r)"
     apt install wireguard iptables
 elif [[ "$OS" = 'fedora' ]]; then
     dnf copr enable jdoss/wireguard
