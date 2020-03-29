7a360c668ebf28cd645c28657f235ef14657f917
randomshell <43271778+randomshell@users.noreply.github.com>
2020-03-29 03:39:12 +0000

    Change Wireguard port from OpenVPN default
    Current port `1194` conflicts with OpenVPN listening port.
    Wireguard docs use port `51820` so we'll use that.

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -52,7 +52,7 @@ read -rp "Server's WireGuard IPv4 " -e -i "$SERVER_WG_IPV4" SERVER_WG_IPV4
 SERVER_WG_IPV6="fd42:42:42::1"
 read -rp "Server's WireGuard IPv6 " -e -i "$SERVER_WG_IPV6" SERVER_WG_IPV6
 
-SERVER_PORT=1194
+SERVER_PORT=51820
 read -rp "Server's WireGuard port " -e -i "$SERVER_PORT" SERVER_PORT
 
 CLIENT_WG_IPV4="10.66.66.2"
